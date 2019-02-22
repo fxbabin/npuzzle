83b03dcb6fea2e652ccb7bfd11bd3ece5c0c69de
terngkub <terngnattapol@gmail.com>
2019-02-22 17:45:49 +0100

    add Graphic class and integrate to the program

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from npuzzle import NPuzzle
 from setting import Setting
+from graphic import Graphic
 
 
 def main():
     setting = Setting()
     npuzzle = NPuzzle(setting)
-    npuzzle.report()
+    Graphic(npuzzle)
+    # npuzzle.report()
 
 if __name__ == "__main__":
     main()
